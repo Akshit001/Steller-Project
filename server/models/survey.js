605ba767e1dab7f4e6a917d981c7cd229ac8bdf2
Bosohyun <kbh900220@gmail.com>
2021-11-10 17:11:38 -0500

    form modification
    T/F -> M/C

--- server/models/survey.js
+++ server/models/survey.js
@@ -3,15 +3,39 @@ let mongoose = require('mongoose');
 //create a model class
 let surveyModel = mongoose.Schema({
   title: String,
+  writerId: String,   // will be used to show the writer in the survey list
+  startDate : Date,
+  endDate: Date,
+
   question1: String,
   question2: String,
   question3: String,
   description: String,
-  writer: String,
   isactive: Boolean,
+
+  QuestionObject1: 
+  { 
+      Question : String, 
+      Options: [String], 
+  },    
+  QuestionObject2: 
+  { 
+      Question : String, 
+      Options: [String],
+  },
+  QuestionObject3:   
+  { 
+      Question : String, 
+      Options: [String], 
+  },    
+  QuestionObject3:   
+  { 
+      Question : String, 
+      Options: [String], 
+  }    
 },
 {
-  collection: "Survey"
+  collection: 'Surveys'
 });
 
 module.exports = mongoose.model('Survey', surveyModel);
