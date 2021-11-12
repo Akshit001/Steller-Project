df196b6735e89473ff9f951693c48c4d528f4d3a
Bosohyun <kbh900220@gmail.com>
2021-11-12 14:50:58 -0500

    merge add edit

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/add', function(req, res, next) {
-  res.render('createsurvey/add', { title: 'Survey' });
+  res.render('createsurvey/add_edit', { title: 'Survey' });
 });
 
 
