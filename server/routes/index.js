1630a564fada782fc435eb82718699dfbac8afd5
Bosohyun <kbh900220@gmail.com>
2021-11-19 09:57:08 -0500

    Authentication
    Complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,32 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
+router.get('/', indexController.displayHomePage);
+
+/* GET home page. */
+router.get('/home', indexController.displayHomePage);
+
+
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/*Post route for processing the login page*/
+router.post('/login', indexController.processLoginPage);
+
+
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/*Post route for processing the Register page*/
+router.post('/register', indexController.processRegisterPage);
+
+
+
+/* GET to perform UserLogout*/
+router.get('/logout', indexController.performLogout);
 
 
 module.exports = router;
